0edc3ef77b50d677eb6b27a04c0cb2c77b4807e9
Darya3em3 <em000303@g.bstu.by>
2023-05-25 05:27:54 +0300

    feat: add footer section
    Добавлена секция подвал

--- calc.js
+++ calc.js
@@ -1,12 +1,10 @@
- (function(s, t, e, p, f, o, r, m) {
+    (function(s, t, e, p, f, o, r, m) {
         s[t] = s[t] || {};
-        s[t][1219612715] = {
-            id: "T43xwUj",
-            rnd: 1219612715
+        s[t][1226525953] = {
+            id: "dNwU0jN",
+            rnd: 1226525953
         };
         e.async = true;
         e.src = p + f;
         document[m](o)[r](e)
-    }(window,"stepFORM_params",document.createElement("script"),
-    document.location.protocol==="https:"?"https:":"http:","//u040844.stepform.io/api.js?id=T43xwUj",
-    "head","appendChild","querySelector"));
+    }(window,"stepFORM_params",document.createElement("script"),document.location.protocol==="https:"?"https:":"http:","//u041236.stepform.io/api.js?id=dNwU0jN","head","appendChild","querySelector"));
